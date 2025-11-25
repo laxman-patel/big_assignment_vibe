6d20cc4dca939dca3f8d1e86c058488092a0d6ae
Puranjay <puranjaymarothia@gmail.com>
2025-11-25 22:20:41 +0530

    rds chnages

--- dockerpush.sh
+++ dockerpush.sh
@@ -1,25 +1,25 @@
 DOCKER_USER="puranjay3"
 
 # Auth Service
-docker build -t $DOCKER_USER/auth-service:latest ./services/auth-service
-docker push $DOCKER_USER/auth-service:latest
+docker build -t $DOCKER_USER/auth-service:v2 ./services/auth-service
+docker push $DOCKER_USER/auth-service:v2
 
 # Post Service
-docker build -t $DOCKER_USER/post-service:latest ./services/post-service
-docker push $DOCKER_USER/post-service:latest
+docker build -t $DOCKER_USER/post-service:v2 ./services/post-service
+docker push $DOCKER_USER/post-service:v2
 
 # Media Service
-docker build -t $DOCKER_USER/media-service:latest ./services/media-service
-docker push $DOCKER_USER/media-service:latest
+docker build -t $DOCKER_USER/media-service:v2 ./services/media-service
+docker push $DOCKER_USER/media-service:v2
 
 # Feed Service
-docker build -t $DOCKER_USER/feed-service:latest ./services/feed-service
-docker push $DOCKER_USER/feed-service:latest
+docker build -t $DOCKER_USER/feed-service:v2 ./services/feed-service
+docker push $DOCKER_USER/feed-service:v2
 
 # Analytics Service
-docker build -t $DOCKER_USER/analytics-service:latest ./services/analytics-service
-docker push $DOCKER_USER/analytics-service:latest
+docker build -t $DOCKER_USER/analytics-service:v2 ./services/analytics-service
+docker push $DOCKER_USER/analytics-service:v2
 
 # Frontend UI
-docker build -t $DOCKER_USER/frontend-ui:latest ./services/frontend-ui
-docker push $DOCKER_USER/frontend-ui:latest
+docker build -t $DOCKER_USER/frontend-ui:v2 ./services/frontend-ui
+docker push $DOCKER_USER/frontend-ui:v2
